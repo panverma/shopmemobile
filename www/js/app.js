7514a4485aa0a6fff29f5e2e5296e79dcdda284c
ManiAmuthan <maniamuthann@gmail.com>
2016-06-05 03:16:52 +0800

    beacon day

--- www/js/app.js
+++ www/js/app.js
@@ -242,6 +242,7 @@ app.controller('MainController', function($rootScope, $scope, $location,
     $scope.prodPage_showStrBtn = true;
     $scope.isIntrested = false;
     $scope.showBackBtn = false;
+    $scope.masterUrl = "http://shopme-epamershackers.rhcloud.com/";
   $scope.swiped = function(direction) {
     alert('Swiped ' + direction);
   };
@@ -277,42 +278,6 @@ app.controller('MainController', function($rootScope, $scope, $location,
     alert('Congrats you scrolled to the end of the list!');
   };
 
-  //
-  // Right Sidebar
-  //
-  $scope.chatUsers = [
-    { name: 'Carlos  Flowers', online: true },
-    { name: 'Byron Taylor', online: true },
-    { name: 'Jana  Terry', online: true },
-    { name: 'Darryl  Stone', online: true },
-    { name: 'Fannie  Carlson', online: true },
-    { name: 'Holly Nguyen', online: true },
-    { name: 'Bill  Chavez', online: true },
-    { name: 'Veronica  Maxwell', online: true },
-    { name: 'Jessica Webster', online: true },
-    { name: 'Jackie  Barton', online: true },
-    { name: 'Crystal Drake', online: false },
-    { name: 'Milton  Dean', online: false },
-    { name: 'Joann Johnston', online: false },
-    { name: 'Cora  Vaughn', online: false },
-    { name: 'Nina  Briggs', online: false },
-    { name: 'Casey Turner', online: false },
-    { name: 'Jimmie  Wilson', online: false },
-    { name: 'Nathaniel Steele', online: false },
-    { name: 'Aubrey  Cole', online: false },
-    { name: 'Donnie  Summers', online: false },
-    { name: 'Kate  Myers', online: false },
-    { name: 'Priscilla Hawkins', online: false },
-    { name: 'Joe Barker', online: false },
-    { name: 'Lee Norman', online: false },
-    { name: 'Ebony Rice', online: false }
-  ];
-
-  //
-  // 'Forms' screen
-  //
-  $scope.rememberMe = true;
-  $scope.email = 'me@example.com';
 
   $scope.login = function() {
     alert('You submitted the login form');
@@ -358,8 +323,29 @@ app.controller('MainController', function($rootScope, $scope, $location,
   //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
   $scope.iAmIntrested = function(){
     $scope.isIntrested = true;
-    //$http.post();
+    /*
+    $http.post("http://example.appspot.com/rest/app", {"foo":"bar"})
+    .success(function(data, status, headers, config) {
+        $scope.data = data;
+    }).error(function(data, status, headers, config) {
+        $scope.status = status;
+    });
+    //*/
 
   };
   //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  $scope.getProductOfferList = function(){
+    var reqObj = new Object();
+    reqObj.reqType = "PRODUCT";
+    reqObj.beacons = $scope.beaconArr;
+    reqObj.storeid = null;
+    $http.post($scope.masterUrl+"view/", reqObj)
+    .success(function(data, status, headers, config) {
+        $scope.data = data;
+    }).error(function(data, status, headers, config) {
+        $scope.status = status;
+    });
+  };
+  //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  $scope.getProductOfferList();
 });
